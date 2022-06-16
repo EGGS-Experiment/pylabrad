3bcb3b410cedafb34e1a90e68201493c05fbe8d1
Clayton Ho <claytonho@g.ucla.edu>
2022-06-16 05:45:57 -0700

    improved logging configurability
    fixed logging with node; node now logs perfectly-ish

--- labrad/logging.py
+++ labrad/logging.py
@@ -1,5 +1,6 @@
-import logging
+import os
 import sys
+import logging
 
 __all__ = ["labradLogFormatter", "_LoggerWriter", "setupLogging"]
 
@@ -26,65 +27,96 @@ class _LoggerWriter:
         self.level(sys.stderr)
 
 
-def setupLogging(logger_name, sender=None):
+def setupLogging(
+        logger_name, sender=None, extraDict=None,
+        logHandlers=None, log_level=logging.DEBUG,
+        syslog=True, syslog_rfc='5424',
+        syslog_socket=(os.environ['LABRADHOST'], os.environ['EGGS_LABRAD_SYSLOG_PORT'])
+    ):
     """
     Sets up the logger.
     Arguments:
-        logger_name : the name of the logger to set up.
-        sender      : the sender.
+        logger_name     : the name of the logger to set up.
+        sender          : the sender.
+        extraDict       : the logging dict to send with each log.
+        logHandlers     : extra handlers to send the logger.
+        log_level       : the default logging level.
+        syslog          : whether to create syslog handlers.
+        syslog_rfc      : the RFC standard for syslog.
+        syslog_socket   : the syslog socket to log to if syslog is True.
     """
-    from os import environ
     from socket import SOCK_STREAM, gethostname
     from logging.handlers import SysLogHandler
-    from rfc5424logging import Rfc5424SysLogHandler
-    from rfc5424logging.adapter import Rfc5424SysLogAdapter
 
-    # create logger
-    logging.basicConfig(level=logging.DEBUG, handlers=None)
+    # get default arguments
+    if (sender is None) and (extraDict is None):
+        raise Exception("Either sender or extraDict must be specified.")
+
+    # create and configure logger
+    logging.basicConfig(level=log_level, handlers=None)
     logger = logging.getLogger(logger_name)
 
     # don't propagate log events to root logger
     logger.propagate = False
 
-    # check if logger already has handlers
+    # check if logger already has handlers; only keep loggers with same name
     handlers = {}
     for name, logger_class in logging.Logger.manager.loggerDict.items():
         if isinstance(logger_class, logging.Logger) and (name == logger_name):
             for log_handler in logger_class.handlers:
                 handlers[logger_name] = log_handler
 
-    # only add handlers if we don't already have some
-    if len(handlers) == 0:
-        # create syslog handler
-        syslog_socket = (environ['LABRADHOST'], int(environ['EGGS_LABRAD_SYSLOG_PORT']))
-        syslog_handler = SysLogHandler(address=syslog_socket)
-        syslog_handler.setFormatter(labradLogFormatter)
-
-        # create console handler
-        console_handler = logging.StreamHandler(sys.stdout)
-        console_handler.setFormatter(labradLogFormatter)
-
-        # create rfc5424 handler
-        loki_handler = Rfc5424SysLogHandler(
-            address=('192.168.1.48', 1514),
-            socktype=SOCK_STREAM,
-            enterprise_id=88888
-        )
-
-        # add handlers
-        logger.addHandler(syslog_handler)
-        logger.addHandler(console_handler)
-        logger.addHandler(loki_handler)
 
     # create custom loghandler so logging labels are specific to each sender
-    extraDict = {
-        'sender_host': gethostname(),
-        'sender_name': sender.__class__.__name__,
-    }
+    if extraDict is None:
+        extraDict = {
+            'sender_host': gethostname(),
+            'sender_name': sender.__class__.__name__,
+        }
 
+    # turn extraDict into structured_data for rfc5424
     structured_data = {'sender': extraDict.copy()}
     extraDict.update({'structured_data': structured_data})
 
-    # set logger as instance variable
-    logger_adapter = Rfc5424SysLogAdapter(logger, extraDict)
-    return logger_adapter
+    # only add handlers if we don't already have some
+    if (len(handlers) == 0):
+        # create console handler
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(labradLogFormatter)
+        logger.addHandler(console_handler)
+
+        if syslog:
+            # create syslog handler
+            if syslog_rfc == '3164':
+                syslog_handler = SysLogHandler(address=syslog_socket)
+                syslog_handler.setFormatter(labradLogFormatter)
+                logger.addHandler(syslog_handler)
+
+            # create rfc5424 handler
+            if syslog_rfc == '5424':
+                try:
+                    from rfc5424logging import Rfc5424SysLogHandler
+                    syslog5424_handler = Rfc5424SysLogHandler(
+                        address=syslog_socket,
+                        socktype=SOCK_STREAM,
+                        enterprise_id=88888
+                    )
+                    logger.addHandler(syslog5424_handler)
+                except ImportError:
+                    print("Error: RFC5424 syslog handler module is not installed.")
+                except Exception as e:
+                    print(e)
+                    print("Error: unable to create RFC5424 syslog handler.")
+
+    # adapt logger and return
+    if syslog_rfc == '5424':
+        try:
+            from rfc5424logging.adapter import Rfc5424SysLogAdapter
+            logger_adapter = Rfc5424SysLogAdapter(logger, extraDict)
+            return logger_adapter
+        except ImportError:
+            print("Error: RFC5424 syslog handler module is not installed.")
+        except Exception as e:
+            print("Error: unable to create RFC5424 syslog handler.")
+    elif syslog_rfc == '3164':
+        return logger
